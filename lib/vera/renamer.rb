e5d3361f6722189d02f6abc6239c445f8b025f06
David Cortés <david.cortes.18@gmail.com>
2021-02-28 14:12:55 +0100

    added renamer command

---
+++ lib/vera/renamer.rb
@@ -0,0 +1,40 @@
+module Vera
+  class Renamer
+    def self.execute(options)
+      unless Exiftool.is_installed?
+        puts """
+          You need to install exiftool to use vera. You can run the following command to do it:
+          bash <(curl -Ls https://git.io/JtbuH)
+        """
+        return
+      end
+      path = options.path
+      path = Dir.pwd if options.path.nil?
+      path = File.expand_path(path)
+      rename path
+    end
+
+    def self.rename path
+      rename_files_with_date path, "ARW",  "DateTimeOriginal"
+      rename_files_with_date path, "JPG",  "DateTimeOriginal"
+      rename_files_with_date path, "HEIC", "DateTimeOriginal"
+      rename_files_with_date path, "MOV",  "CreationDate"
+    end
+
+    def self.rename_files_with_date path, ext, date_property
+      puts "✅ Adding creation date to #{ext} files in the filename"
+      files = Lister.all_files(path, ext)
+      files = Renamer.exclude_already_renamed_files(files)
+      if files.empty?
+        puts "No files to rename for #{ext} file type."
+        return
+      end
+      files_string = files.map { |f| "\"#{f[:path]}\"" }.join(" ")
+      puts "❌Something went wrong" unless Exiftool.change_filename_with_date(date_property, files_string)
+    end
+
+    def self.exclude_already_renamed_files files
+      files.select { |f| f[:filename].match(/\d+\-\d+\-\d+\-/) == nil }
+    end
+  end
+end
